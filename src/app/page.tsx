eba71a0bc06c3ebbacb4069f24de4d02eae47700
shaan-ram <113085350+shaan-ram@users.noreply.github.com>
2024-07-05 18:39:29 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './login/page';
+
+const RootPage = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default RootPage;
